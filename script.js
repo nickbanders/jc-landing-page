bd579492bcbd437e2f80faf68acea1570af269a1
Nick Anders <92825392+nickbanders@users.noreply.github.com>
2022-10-14 15:10:13 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-function(){
+(function(){
 	let stylesheet = document.createElement('link');
-		stylesheet.setAttribute('href','https://raw.githubusercontent.com/nickbanders/jc-landing-page/main/style.css');
+		stylesheet.setAttribute('href','https://hou-public.s3.amazonaws.com/shared/shop-jaygup.css');
 		stylesheet.setAttribute('rel','stylesheet');
 		stylesheet.setAttribute('type','text/css');
 	
@@ -14,33 +14,3 @@ function(){
 	document.querySelector('head').appendChild(stylesheet);
 	document.querySelector('.custom-content').prepend(video);	
 }());
-
-let slideIndex = 1;
-showSlides(slideIndex);
-
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-}
-
