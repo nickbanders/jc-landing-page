3b4877f6552cbc8f296cf4abd328fac6cb1dfd82
Nick Anders <92825392+nickbanders@users.noreply.github.com>
2022-10-14 14:57:03 -0500

    Update script.js

--- script.js
+++ script.js
@@ -26,3 +26,20 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
 }
+
+(function(){
+	let stylesheet = document.createElement('link');
+		stylesheet.setAttribute('href','https://hou-public.s3.amazonaws.com/shared/shop-jaygup.css');
+		stylesheet.setAttribute('rel','stylesheet');
+		stylesheet.setAttribute('type','text/css');
+	
+	let video = document.createElement('video');
+		video.setAttribute('src','https://hou-public.s3.amazonaws.com/shared/josh_christopher_x_rockets_line.mp4');
+		video.classList.add('hero');
+		video.autoplay = 'autoplay';
+		video.muted = 'muted';
+		video.controls = 'controls';
+
+	document.querySelector('head').appendChild(stylesheet);
+	document.querySelector('.custom-content').prepend(video);	
+}());
