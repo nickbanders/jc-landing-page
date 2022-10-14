42433866300da9f4f0148f6ad1702633be610ca0
Nick Anders <92825392+nickbanders@users.noreply.github.com>
2022-10-14 15:11:28 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,28 @@
-(function(){
-	let stylesheet = document.createElement('link');
-		stylesheet.setAttribute('href','https://hou-public.s3.amazonaws.com/shared/shop-jaygup.css');
-		stylesheet.setAttribute('rel','stylesheet');
-		stylesheet.setAttribute('type','text/css');
-	
-	let video = document.createElement('video');
-		video.setAttribute('src','https://hou-public.s3.amazonaws.com/shared/josh_christopher_x_rockets_line.mp4');
-		video.classList.add('hero');
-		video.autoplay = 'autoplay';
-		video.muted = 'muted';
-		video.controls = 'controls';
+let slideIndex = 1;
+showSlides(slideIndex);
 
-	document.querySelector('head').appendChild(stylesheet);
-	document.querySelector('.custom-content').prepend(video);	
-}());
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
